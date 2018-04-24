01de27af44a2a6f00e3e5b2aed6865bdfb0c4668
Denny <denny.kluge@gmail.com>
2018-04-24 08:16:20 +0200

    extended test

--- src/test/java/DivisibleNbTest.java
+++ src/test/java/DivisibleNbTest.java
@@ -12,6 +12,7 @@ public class DivisibleNbTest {
         Assert.assertTrue(DivisibleNb.isDivisible(12, 4, 3));
         Assert.assertTrue(DivisibleNb.isDivisible(3, 1, 3));
         Assert.assertTrue(DivisibleNb.isDivisible(12, 2, 6));
+        Assert.assertTrue(DivisibleNb.isDivisible(1, 1, 1));
         Assert.assertFalse(DivisibleNb.isDivisible(100, 5, 3));
         Assert.assertFalse(DivisibleNb.isDivisible(12, 7, 5));
     }
