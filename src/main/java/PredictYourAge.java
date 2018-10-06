2a063d7b7331b0d9df01658aaff9725afd2a3a1e
diskostu <denny.kluge@gmail.com>
2018-10-06 23:17:31 +0200

    new game

---
+++ src/main/java/PredictYourAge.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+/**
+ * https://www.codewars.com/kata/5aff237c578a14752d0035ae/train/java
+ *
+ * @author https://github.com/diskostu
+ */
+class PredictYourAge {
+    static int predictAge(final int... ages) {
+        final int sum = Arrays.stream(ages)
+                              .map(operand -> operand * operand)
+                              .sum();
+        return Math.toIntExact((long) Math.sqrt(sum) / 2);
+    }
+}
---
+++ src/test/java/PredictYourAgeTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PredictYourAgeTest {
+
+    @Test
+    public void testMethod() {
+        Assert.assertEquals(86, PredictYourAge.predictAge(65, 60, 75, 55, 60, 63, 64, 45));
+        Assert.assertEquals(79, PredictYourAge.predictAge(32, 54, 76, 65, 34, 63, 64, 45));
+    }
+}
