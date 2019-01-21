37e4cc0c107750667baf19d0fb925952c6e2209d
Denny <denny.kluge@gmail.com>
2019-01-21 13:05:08 +0000

    new challenge added

---
+++ src/main/java/TwoToOne.java
@@ -0,0 +1,17 @@
+import java.util.stream.Collectors;
+
+/**
+ * https://www.codewars.com/kata/two-to-one/train/java
+ *
+ * @author https://github.com/diskostu
+ */
+class TwoToOne {
+    static String longest(String s1, String s2) {
+        return (s1 + s2).chars()
+                .mapToObj(i -> (char) i)
+                .distinct()
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+}
